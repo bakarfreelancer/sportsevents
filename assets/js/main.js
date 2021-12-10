ce7f0ac9058b4a3357f9fb90eee4fb93e84626be
bakarfreelancer <bakarfreelancer96@gmail.com>
2021-12-10 21:19:07 +0500

    edit player modal added

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,24 +24,45 @@
 
 // EDIT GAME MODAL
 (function () {
-  let allPlayerEditBtn = document.querySelectorAll(".editPlayer");
-  for (let i = 0; i < allPlayerEditBtn.length; i++) {
-    allPlayerEditBtn[i].addEventListener("click", function () {
-      // let editModal = document.getElementById("editPlayerModal");
+  let allGameEditBtn = document.querySelectorAll(".editGame");
+  for (let i = 0; i < allGameEditBtn.length; i++) {
+    allGameEditBtn[i].addEventListener("click", function () {
       let game = this.parentElement.parentElement.childNodes;
-      // let gameCurrentTime = game[5].textContent;
-      // gameCurrentTime = Date.parse(gameCurrentTime);
-      // let dateToBeDisplayed = new Date(gameCurrentTime * 1000);
 
-      // console.log(dateToBeDisplayed);
       let filterdData = [
+        game[1].textContent,
         game[3].textContent,
         game[5].textContent,
         game[7].textContent,
       ];
-      document.querySelector(".gameName").value = filterdData[0];
-      document.querySelector(".gameDate").value = filterdData[1];
-      document.querySelector(".gameVenue").value = filterdData[2];
+      document.querySelector(".gameId").value = filterdData[0];
+      document.querySelector(".gameName").value = filterdData[1];
+      document.querySelector(".gameDate").value = filterdData[2];
+      document.querySelector(".gameVenue").value = filterdData[3];
+      console.log(filterdData[0]);
+    });
+  }
+})();
+
+// EDIT PLAYER MODAL
+(function () {
+  let allPlayerEditBtn = document.querySelectorAll(".editPlayer");
+  for (let i = 0; i < allPlayerEditBtn.length; i++) {
+    allPlayerEditBtn[i].addEventListener("click", function () {
+      let game = this.parentElement.parentElement.childNodes;
+
+      let filterdData = [
+        game[1].textContent,
+        game[3].textContent,
+        game[5].textContent,
+        game[7].textContent,
+      ];
+      console.log(filterdData);
+      document.querySelector(".playerId").value = filterdData[0];
+      document.querySelector(".playerName").value = filterdData[1];
+      document.querySelector(".address").value = filterdData[2];
+      document.querySelector(".playerContact").value = filterdData[3];
+      console.log(filterdData[0]);
     });
   }
 })();
