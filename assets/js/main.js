1a0fa5142de50512546fdf5bd97cbacb9189a006
bakarfreelancer <bakarfreelancer96@gmail.com>
2021-12-10 11:38:31 +0500

    edit player js logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,3 +21,26 @@
     );
   });
 })();
+
+// EDIT GAME MODAL
+(function () {
+  let allPlayerEditBtn = document.querySelectorAll(".editPlayer");
+  for (let i = 0; i < allPlayerEditBtn.length; i++) {
+    allPlayerEditBtn[i].addEventListener("click", function () {
+      let editModal = document.getElementById("editPlayerModal");
+      let game = this.parentElement.parentElement.childNodes;
+      let gameCurrentTime = game[5].textContent;
+      gameCurrentTime = Date.parse(gameCurrentTime);
+      let dateToBeDisplayed = new Date(gameCurrentTime * 1000);
+      console.log(dateToBeDisplayed);
+      let filterdData = [
+        game[3].textContent,
+        game[5].textContent,
+        game[7].textContent,
+      ];
+      document.querySelector(".gameName").value = filterdData[0];
+      document.querySelector(".gameDate").value = filterdData[1];
+      document.querySelector(".gameVenue").value = filterdData[2];
+    });
+  }
+})();
